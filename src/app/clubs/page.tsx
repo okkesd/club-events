5922e80a58788da4f27ff02826df2391b35bfd6b
Okkes <okkesdonbaloglu25@gmail.com>
2026-02-27 10:50:35 +0300

    deploy trigger

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -31,7 +31,7 @@ export default function ClubsDirectory() {
     }
   };
 
-  // trigger merge
+  // trigger merge again
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-950 pb-20 transition-colors duration-300">
       
